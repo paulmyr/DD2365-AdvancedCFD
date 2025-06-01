caaca86f308524fc75699ca85e496f9ab8514e60
Paul Mayer <p@mayer-zuffenhausen.de>
2025-06-01 13:46:34 +0200

    final report

--- project/report/auto/report.el
+++ project/report/auto/report.el
@@ -4,23 +4,23 @@
  "report"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("article" "a4paper" "10pt")))
+                     '(("article" "a4paper" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("geometry" "a4paper" "bindingoffset=0.2in" "left=1in" "right=1in" "top=1in" "bottom=1in" "footskip=.25in") ("biblatex" "backend=biber" "sortcites=true" "style=alphabetic" "eprint=true" "backref=true") ("glossaries" "acronym") ("euscript" "") ("amsmath" "") ("amssymb" "") ("amstext" "") ("amsthm" "") ("mdframed" "") ("mathtools" "") ("enumerate" "") ("graphicx" "pdftex") ("subcaption" "") ("hyperref" "") ("tikz" "") ("listings" "") ("algorithm" "") ("algpseudocode" "") ("wasysym" "") ("titling" "") ("titlesec" "") ("fancyhdr" "nocheck") ("lastpage" "") ("textpos" "absolute" "overlay") ("kantlipsum" "") ("todonotes" "colorinlistoftodos" "prependcaption" "textsize=tiny")))
+                     '(("inputenc" "utf8") ("geometry" "a4paper" "bindingoffset=0.2in" "left=1in" "right=1in" "top=1in" "bottom=1in" "footskip=.25in") ("biblatex" "backend=biber" "sortcites=true" "style=alphabetic" "eprint=true" "backref=true") ("glossaries" "acronym") ("euscript" "") ("amsmath" "") ("amssymb" "") ("amstext" "") ("amsthm" "") ("acronym" "printonlyused" "withpage") ("mdframed" "") ("mathtools" "") ("enumerate" "") ("graphicx" "pdftex") ("subcaption" "") ("hyperref" "") ("tikz" "") ("listings" "") ("algorithm" "") ("algpseudocode" "") ("wasysym" "") ("titling" "") ("titlesec" "") ("fancyhdr" "nocheck") ("lastpage" "") ("textpos" "absolute" "overlay") ("kantlipsum" "") ("todonotes" "colorinlistoftodos" "prependcaption" "textsize=tiny")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "article"
-    "art10"
+    "art12"
     "inputenc"
     "geometry"
     "biblatex"
@@ -30,6 +30,7 @@
     "amssymb"
     "amstext"
     "amsthm"
+    "acronym"
     "mdframed"
     "mathtools"
     "enumerate"
@@ -56,8 +57,26 @@
     "I"
     "E"
     "Prob")
+   (LaTeX-add-labels
+    "eq:galerkinfem"
+    "eq:curl"
+    "eq:tripledec"
+    "sec:result_grid"
+    "fig:re10000-rs16"
+    "fig:re10000-rs32"
+    "fig:re10000-rs64"
+    "fig:re10000-rs128"
+    "fig:rr-resolution"
+    "fig:re16vs64"
+    "fig:re128vs64"
+    "sec:result_reynolds"
+    "fig:re1000-rs64"
+    "fig:re10000-rs64-2"
+    "fig:re100000-rs64"
+    "fig:re1000000-rs64"
+    "fig:rr-reynoldnumber")
    (LaTeX-add-bibliographies
-    "bibliographie")
+    "bibliography")
    (LaTeX-add-amsthm-newtheorems
     "definition"
     "lemma"
